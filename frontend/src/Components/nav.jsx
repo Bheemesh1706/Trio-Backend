254e9c8e10da43982460e794ecadae12c92d708e
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-08 17:14:32 +0530

    Padding fixed

--- frontend/src/Components/nav.jsx
+++ frontend/src/Components/nav.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-
+import {
+  Link
+} from "react-router-dom";
 export default function Navbar(){
 
     return (<div className ="navStyle">
             <nav>
                 <ul>
                         <li>
-                            <a href='/login'>Login</a>
+                           <Link exact to='/login'><p>Login</p></Link>
                         </li>
                         <li>
-                            <a href='/'>Register</a>
+                            <Link exact to='/'><p>Register</p></Link>
                         </li>
                 </ul>
             </nav>
