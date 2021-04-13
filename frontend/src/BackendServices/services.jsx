ede2cd4b48dae723d83acc2f8a6625f1b37ee66e
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-13 17:43:38 +0530

    usefrom implementaion done

--- frontend/src/BackendServices/services.jsx
+++ frontend/src/BackendServices/services.jsx
@@ -16,35 +16,5 @@ import{API_HOST} from '../Components/config'
             catch(error){console.log(error) } 
   }
 
-  const dataValidation = (data)=> {
-                try
-                {
-                   const regex_name= /^[a-zA-Z0-9_.]{8,15}$/ 
-                   const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                   const regex_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-                   var error_message=[]
-                            
-                            if (!regex_name.test(data.username))
-                            {
-                                 error_message.push({error: "Username not matched with rules",field: 'Username' })
-                               
-                            }
-                            if (!regex_password.test(data.password))
-                            {
-                                error_message.push({error: "Password not matched with rules",field: 'Password' })
-                            }
-                            if(!regex_email.test(data.email))
-                            {
-                                error_message.push({error: "Email not matched with rules",field: 'Email' })
-                            }
-                           
-                            return error_message;
-                                  
-                }
-                catch(error)
-                {
-                        console.log(error)
-                }
-  }
 
-  export {sendData,dataValidation}
+  export {sendData}
--- frontend/src/LoginComponent/form.jsx
+++ frontend/src/LoginComponent/form.jsx
@@ -1,55 +1,53 @@
-import React,{useState}from "react";
-import {sendData,dataValidation} from '../BackendServices/services'
+import React,{useEffect}from "react";
+import {sendData} from '../BackendServices/services';
+import {useLocation} from 'react-router-dom';
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+
+
+const schema =yup.object().shape({
+    Email: yup.string().email().required(),
+    Username: yup.string().min(8).max(15).matches(/^[a-zA-Z0-9_]+$/,'Valid Inputs are charecter number and ("_")').required(),
+    Password: yup.string().min(6).max(15).required()
+})
 
 function Form ({page})
 {
-    const  [formdata,setValue] = useState({});
-    const  [errordata,setErrorData] = useState({});
+   
+   const  {register,handleSubmit,formState: { errors } } = useForm({
+       resolver: yupResolver(schema)
+   })
+   
+    let location = useLocation();
+    
+    useEffect(() => {}, [location]);
 
     function handleLogin(e)
-    {
-       e.preventDefault()
-        console.log('login')
-        sendData(formdata)
+    {console.log('login')
+        //sendData(formdata)
     }
 
     function handleRegister(e)
     {
-       e.preventDefault()
-
-       const  error_message  = dataValidation(formdata)
-      
-       if  (error_message.length === 0)
-         {
-             
-            //  sendData(formdata)
-            console.log('Success')
-         }
-        else
-        {     
-                error_message.forEach( (e)=> { setErrorData( p =>({...p, [e.field]: e.error})) } )
-               
-        }
-
-                
+     window.location.replace('/');
+       console.log(e)
+       console.log('register')         
     }
 
 
     return(<div className="right">
-        <form onSubmit={page==='Register'? handleRegister:handleLogin}>
+        <form onSubmit={ handleSubmit(page==='Register'? handleRegister:handleLogin)}>
             <label>{page=='Register'?'Signup':'Login'}</label>
-            {page==='Register'&&<input type='email' id='email' placeholder="Email" name='Email'
-                                value = {formdata.Email}
-                                onChange={event => setValue(d =>({...d,email: event.target.value}))}/>}
-                <div className="error">{errordata.Email}</div>
-            <input type="text" id="uname" placeholder="UserName" name='Username'
-                                value = {formdata.Username}
-                                onChange={event => setValue(d =>({...d,username: event.target.value}))} />
-                <div className="error">{errordata.Username}</div>
-            <input type="password" id="password" placeholder="Password" name='Password'
-                                value = {formdata.Password}
-                                onChange={event => setValue(d =>({...d,password: event.target.value}))}/>
-                <div className="error">{errordata.Password}</div>
+            {page==='Register'&&<input type='email' id='email' placeholder="Email" name='Email' {...register('Email')}/>}
+            {page=='Register'&&<p className="error1">{errors.Email?.message}</p>}
+            
+            <input type="text" id="uname" placeholder="UserName" name='Username' {...register('Username')}/>
+            <p className="error2">{errors.Username?.message}</p>  
+            
+            <input type="password" id="password" placeholder="Password" name='Password' {...register('Password')}/>
+             <p className="error3">{errors.Password?.message}</p>   
             <button type='submit'>{page}</button>
         </form>
     </div>
