02ee81a0281b6c5d5da147a4317c5983756780bb
Bheemesh1706 <bheemeshbala.71@gmail.com>
2021-04-27 23:04:32 +0530

    Register to login redirecct completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,12 @@ class UsersController < ApplicationController
   def show
     render json: @user
   end
-
+ 
   # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: {success_message: "User Created Sucessfully"}, status: 200
     else
       render json: @user.errors.full_messages, status: 420
     end
